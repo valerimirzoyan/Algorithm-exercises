5b4adf30c1314b9ac1822ef3fd066d121f66dd4d
Valeri <79400913+valerimirzoyan@users.noreply.github.com>
2025-03-22 14:50:02 +0400

    Create SingleList.js

---
+++ SingleList.js
@@ -0,0 +1,44 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    // Method to add a new node at the end
+    append(data) {
+        let newNode = new Node(data);
+        if (!this.head) {
+            this.head = newNode;
+            return;
+        }
+        let current = this.head;
+        while (current.next) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    // Method to display the linked list
+    display() {
+        let current = this.head;
+        let result = "";
+        while (current) {
+            result += current.data + " -> ";
+            current = current.next;
+        }
+        console.log(result + "null");
+    }
+}
+
+// Example usage
+let list = new LinkedList();
+list.append(10);
+list.append(20);
+list.append(30);
+list.display(); // Output: 10 -> 20 -> 30 -> null
