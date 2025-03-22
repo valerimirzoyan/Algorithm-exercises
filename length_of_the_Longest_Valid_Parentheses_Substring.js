c7a655d4ae0be65ac72acc0fa0c107f769d29593
Valeri <79400913+valerimirzoyan@users.noreply.github.com>
2025-03-22 15:16:20 +0400

    Create length_of_the_Longest_Valid_Parentheses_Substring.js

---
+++ length_of_the_Longest_Valid_Parentheses_Substring.js
@@ -0,0 +1,24 @@
+function longestValidParentheses(s) {
+    let maxLen = 0;
+    let stack = [];
+    stack.push(-1);
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(') {
+            stack.push(i);
+        } else {
+            stack.pop();
+            if (stack.length === 0) {
+                stack.push(i);
+            } else {
+                maxLen = Math.max(maxLen, i - stack[stack.length - 1]);
+            }
+        }
+    }
+
+    return maxLen;
+}
+
+
+console.log(longestValidParentheses("(()"));
+console.log(longestValidParentheses(")()())"));
